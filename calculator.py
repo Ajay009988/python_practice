961fcd2e9701ddba79e0075f3df4c894246994c7
ajay <ajayyadav36882300@gmail.com>
2024-03-02 19:07:17 +0530

    recovery

---
+++ calculator.py
@@ -0,0 +1,15 @@
+a=int(input("enter the value of a :"))
+b=int(input("enter the value of b :"))
+c=(input("enter the value of c"))
+if c=="+":
+    print(a+b)
+elif c=="-":
+    print(a-b)
+elif c=="*":
+    print(a*b)
+elif c=="/":
+    print(a/b)
+elif c=="%":
+    print(a%b)
+else:
+    print("invalid number")
